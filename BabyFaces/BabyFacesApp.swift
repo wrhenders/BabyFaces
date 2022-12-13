51d08483f10abb86d0622434587e1f015f0eea17
wrhenders <wrhenders@gmail.com>
2022-12-13 11:47:10 -0600

    Initial Commit

---
+++ BabyFaces/BabyFacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyFacesApp.swift
+//  BabyFaces
+//
+//  Created by Ryan Henderson on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct BabyFacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
